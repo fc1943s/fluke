15d938a68606a15c644675e22a22e26bc8291317
Filipe <fc1943s@gmail.com>
2020-05-16 13:51:06 -0300

    Initial clojure files

---
+++ src/fluke-ui-frontend-clj/src/main/app/client.cljs
@@ -0,0 +1,23 @@
+(ns app.client
+  (:require
+    [com.fulcrologic.fulcro.application :as app]
+    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.dom :as dom]))
+
+(defonce app (app/fulcro-app))
+
+(defsc Root [this props]
+  (dom/div "TODO"))
+
+(defn ^:export init
+  "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
+  []
+  (app/mount! app Root "app")
+  (js/console.log "Loaded"))
+
+(defn ^:export refresh
+  "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
+  []
+  ;; re-mounting will cause forced UI refresh, update internals, etc.
+  (app/mount! app Root "app")
+  (js/console.log "Hot reload"))
