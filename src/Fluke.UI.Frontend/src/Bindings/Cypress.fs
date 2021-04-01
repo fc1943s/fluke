ab4e624a677b247d7ad6b6138d99726b8c2ff5b7
fc1943s <fc1943s@gmail.com>
2021-04-01 00:24:07 -0300

    test fix

---
+++ src/Fluke.UI.Frontend/src/Bindings/Cypress.fs
@@ -0,0 +1,53 @@
+namespace Fluke.UI.Frontend.Bindings
+
+open Fable.Core
+
+
+module Cypress =
+    [<Emit("describe($0, $1)")>]
+    let describe (_title: string) (_fn: unit -> unit) = jsNative
+
+    [<Emit("before($0)")>]
+    let before (_fn: unit -> unit) = jsNative
+
+    type ExpectTo<'T> =
+        abstract contain : 'T -> unit
+
+    type Expect<'T> =
+        abstract ``to`` : ExpectTo<'T>
+
+    [<Emit("expect($0)")>]
+    let expect<'T> (_name: 'T) : Expect<'T> = jsNative
+
+    [<Emit("it($0, $1)")>]
+    let it (_name: string) (_fn: unit -> unit) = jsNative
+
+    module Cy =
+        type Chainable<'T> =
+            abstract should : ('T -> unit) -> unit
+
+        type Chainable2<'T> =
+            abstract should : string -> string -> 'T -> unit
+            abstract click : unit -> unit
+            abstract ``type`` : string -> unit
+            abstract get : string -> Chainable2<'T>
+
+        type Location =
+            abstract pathname : string
+            abstract href : string
+            abstract hash : string
+
+        [<Emit("cy.location()")>]
+        let location () : Chainable<Location> = jsNative
+
+        [<Emit("cy.focused()")>]
+        let focused () : Chainable2<unit> = jsNative
+
+        [<Emit("cy.visit($0)")>]
+        let visit (_url: string) : unit = jsNative
+
+        [<Emit("cy.contains($0, $1)")>]
+        let contains (_text: string) (_options: {| timeout: int |} option) : Chainable2<'T> = jsNative
+
+        [<Emit("cy.get($0)")>]
+        let get (_selector: string) : Chainable2<string> = jsNative
