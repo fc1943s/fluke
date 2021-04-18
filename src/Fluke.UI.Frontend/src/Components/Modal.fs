07b8a7e294900abec8f7aa479acbca31cac1632e
fc1943s <fc1943s@gmail.com>
2021-04-18 18:24:51 -0300

    mobile zoom test

--- src/Fluke.UI.Frontend/src/Components/Modal.fs
+++ src/Fluke.UI.Frontend/src/Components/Modal.fs
@@ -3,6 +3,7 @@ namespace Fluke.UI.Frontend.Components
 open Feliz
 open Fluke.UI.Frontend.Bindings
 open Fable.React
+open Fable.Core.JsInterop
 
 
 module Modal =
@@ -13,6 +14,21 @@ module Modal =
                    OnClose: unit -> unit
                    children: seq<ReactElement> |})
         =
+        let isOpenRef = React.useRef input.IsOpen
+
+        React.useEffect (
+            (fun () ->
+                if input.IsOpen <> isOpenRef.current then
+                    isOpenRef.current <- input.IsOpen
+                    Browser.Dom.window.document?body?style?zoom <- 1
+
+                ),
+            [|
+                box input.IsOpen
+                box isOpenRef
+            |]
+        )
+
         Chakra.modal
             {|
                 isCentered = true
