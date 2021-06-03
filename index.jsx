b636ec9cd83cc4629f5a7ab35c5277b8da7e779b
fc1943s <fc1943s@users.noreply.github.com>
2021-06-03 14:03:31 +0000

    deploy: 9009ab5705eb031007125e5be94ee11834e8d2c1

--- index.jsx
+++ index.jsx
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-
-const Counter = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            //setCounter(previousCounter => previousCounter + 1);
-        }, 100);
-
-        return () => {
-            clearInterval(timer);
-        };
-    }, []);
-
-    return <span color="green">{counter}</span>;
-};
-
-const interval = setInterval(() => {
-    const container = document.getElementById("test1");
-    if (container) {
-        clearInterval(interval);
-        ReactDOM.render(<Counter/>, container);
-    }
-}, 100);
+// import React, {useState, useEffect} from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const Counter = () => {
+//     const [counter, setCounter] = useState(0);
+//
+//     useEffect(() => {
+//         const timer = setInterval(() => {
+//             //setCounter(previousCounter => previousCounter + 1);
+//         }, 100);
+//
+//         return () => {
+//             clearInterval(timer);
+//         };
+//     }, []);
+//
+//     return <span color="green">{counter}</span>;
+// };
+//
+// const interval = setInterval(() => {
+//     const container = document.getElementById("test1");
+//     if (container) {
+//         clearInterval(interval);
+//         ReactDOM.render(<Counter/>, container);
+//     }
+// }, 100);
