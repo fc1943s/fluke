2a7806230b48a9e645573375dd9b08daba05089e
fc1943s <root@fc1943s.dev>
2021-06-16 00:55:34 -0300

    tests

--- src/Fluke.UI.Frontend/src/Components/ShiftListener.fs
+++ src/Fluke.UI.Frontend/src/Components/ShiftListener.fs
@@ -5,11 +5,7 @@ open Fable.React
 open Fluke.UI.Frontend.State
 open Fluke.UI.Frontend.Hooks
 open Fable.Core
-open Feliz.Recoil
-open Browser.Types
-open Feliz.UseListener
 open Fluke.Shared
-open Fluke.UI.Frontend
 open Fluke.UI.Frontend.Bindings
 
 
@@ -17,26 +13,28 @@ module ShiftListener =
 
     [<ReactComponent>]
     let ShiftListener () =
-        let username = Store.useValue Atoms.username
-        let setView = Recoil.useSetStateKeyDefault Atoms.User.view username TempUI.defaultView
-
-        let onKeyDown =
-            Recoil.useCallbackRef
-                (fun _ (e: KeyboardEvent) ->
-                    match e.ctrlKey, e.shiftKey, e.key with
-                    | false, true, "I" ->
-                        JS.log (fun () -> "RouterObserver.onKeyDown() View.Information")
-                        setView View.View.Information
-                    | false, true, "H" -> setView View.View.HabitTracker
-                    | false, true, "P" -> setView View.View.Priority
-                    | false, true, "B" -> setView View.View.BulletJournal
-                    | _ -> ())
-
-        React.useListener.onKeyDown onKeyDown
 
         Listener.useKeyPress
             (fun setter e ->
                 async {
+                    let setView value =
+                        async {
+                            let! username = setter.snapshot.getAsync Atoms.username
+
+                            match username with
+                            | Some username -> setter.set (Atoms.User.view username, (fun _ -> value))
+                            | None -> ()
+                        }
+
+                    match e.ctrlKey, e.shiftKey, e.key with
+                    | false, true, "I" ->
+                        JS.log (fun () -> "RouterObserver.onKeyDown() View.Information")
+                        do! setView View.View.Information
+                    | false, true, "H" -> do! setView View.View.HabitTracker
+                    | false, true, "P" -> do! setView View.View.Priority
+                    | false, true, "B" -> do! setView View.View.BulletJournal
+                    | _ -> ()
+
                     let! shiftPressed = setter.snapshot.getAsync Atoms.shiftPressed
 
                     if e.shiftKey <> shiftPressed then
