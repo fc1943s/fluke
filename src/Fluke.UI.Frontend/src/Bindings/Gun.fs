344add2b582bf19e0fbe9c0e74204b3067f3a9e0
fc1943s <root@fc1943s.dev>
2021-07-06 09:37:19 -0300

    tests

--- src/Fluke.UI.Frontend/src/Bindings/Gun.fs
+++ src/Fluke.UI.Frontend/src/Bindings/Gun.fs
@@ -260,7 +260,7 @@ module Gun =
                     item.Fn (item.Timestamp, item.Data))
              >> Promise.Parallel
              >> Promise.start)
-            {| interval = 1500 |}
+            {| interval = 750 |}
 
     let subscribe (gun: IGunChainReference) fn =
         gun.on
@@ -292,7 +292,7 @@ module Gun =
                     })
              >> Promise.Parallel
              >> Promise.start)
-            {| interval = 1500 |}
+            {| interval = 750 |}
 
     let wrapAtomPath (atomPath: string) =
         let header = $"{nameof Fluke}/"
